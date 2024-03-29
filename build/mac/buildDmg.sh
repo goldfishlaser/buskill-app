cc3d38cc1d057167b3e7efdefff7f597ac61641f
Michael Altfield <michael@michaelaltfield.net>
2024-03-29 16:43:36 -0500

    uninstall -> reinstall -> install

--- build/mac/buildDmg.sh
+++ build/mac/buildDmg.sh
@@ -184,12 +184,14 @@ ls -lah ${cacheDir}
 ${BREW} reinstall --debug --verbose wget-1.24.5.ventura.bottle.tar.gz
 
 ${BREW} uninstall --debug --verbose --ignore-dependencies python
+${BREW} reinstall --debug --verbose build/deps/python-3.12.ventura.bottle.tar.gz
 ${BREW} install --debug --verbose build/deps/python-3.12.ventura.bottle.tar.gz
 ${BREW} install --debug --verbose --ignore-dependencies build/deps/python-3.12.ventura.bottle.tar.gz
 PYTHON_PATH="`find /usr/local/Cellar/python* -type f -wholename *bin/python3.12 | sort -n | uniq | head -n1`"
 
 ${PYTHON_PATH} -m pip list
 ${BREW} uninstall --debug --verbose --ignore-dependencies virtualenv
+${BREW} reinstall --debug --verbose build/deps/virtualenv-20-25.1.ventura.bottle.tar.gz
 ${BREW} install --debug --verbose build/deps/virtualenv-20-25.1.ventura.bottle.tar.gz
 ${BREW} install --debug --verbose --ignore-dependencies build/deps/virtualenv-20-25.1.ventura.bottle.tar.gz
 
